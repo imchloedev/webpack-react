052adf265e3ffdc061dc041f7ed12b825dfb1a04
Eunji Kang <etothej@naver.com>
2023-05-23 21:31:56 +0900

    environment variable set up

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,57 +2,51 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
-const prod = process.env.REACT_APP_NODE_ENV === 'production';
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const plugins = [
-  new HtmlWebpackPlugin({
-    template: './public/index.html',
-  }),
-  new CleanWebpackPlugin(),
-  new webpack.HotModuleReplacementPlugin(),
-];
+module.exports = (env: any) => {
+  const isProd = env.production === true;
 
-if (prod) {
-  plugins.push(
-    new MiniCssExtractPlugin({
-      filename: 'common.css',
-    })
-  );
-}
-
-module.exports = {
-  mode: prod ? 'production' : 'development',
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  }, // 확장자나 경로를 알아서 처리할 수 있도록 설정
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  plugins,
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: ['babel-loader', 'ts-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          prod ? MiniCssExtractPlugin.loader : 'style-loader',
-          'css-loader',
-        ],
-      },
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: './src/index.tsx',
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }, // 확장자나 경로를 알아서 처리할 수 있도록 설정
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: ['babel-loader', 'ts-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          sideEffects: true,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new CleanWebpackPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin({ filename: 'app.css' }),
     ],
-  },
-
-  devServer: {
-    historyApiFallback: true,
-    port: 8080,
-    hot: true,
-  },
+    devServer: {
+      historyApiFallback: true,
+      port: 8080,
+      hot: true,
+    },
+  };
 };
